64c5d48f3636c434470a0ed8ce16f359f562afab
ArthurDelcas <arthur.d10@aluno.ifsc.edu.br>
2026-09-17 11:24:07 -0300

    pilha add

--- libprg/pilha/pilha.c
+++ libprg/pilha/pilha.c
@@ -1,5 +1,27 @@
-//
-// Created by aluno on 17/09/2026.
-//
-
 #include "pilha.h"
+
+void pilha_inicializar(Pilha *p) {
+    p->topo = -1;
+}
+
+int pilha_vazia(Pilha *p) {
+    return p->topo == -1;
+}
+
+int pilha_cheia(Pilha *p) {
+    return p->topo == TAM_MAX - 1;
+}
+
+int pilha_empilhar(Pilha *p, int valor) {
+    if (pilha_cheia(p)) return 0;
+    p->topo++;
+    p->dados[p->topo] = valor;
+    return 1;
+}
+
+int pilha_desempilhar(Pilha *p, int valor) {
+    if (pilha_vazia(p)) return 0;
+    valor = p->dados[p->topo];
+    p->topo--;
+    return 1;
+}
