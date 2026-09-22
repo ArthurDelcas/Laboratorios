c99d600062d10211af9c3f9c20395a602774aa57
ArthurDelcas <arthur.d10@aluno.ifsc.edu.br>
2026-09-22 10:50:17 -0300

    att

--- lab_7/src/lista_encadeada.c
+++ lab_7/src/lista_encadeada.c
@@ -3,6 +3,11 @@
 #include "/home/aluno/CLionProjects/Laboratorios/libprg/libprg.h"
 //  ↑   informo o caminho do header com a definição da funções no libprg.h
 
+typedef struct No {
+    int valor;
+    struct No* proximo;
+};
+
 Lista* criar_lista() {
     Lista* lista = (Lista*) malloc(sizeof(Lista));
     if (lista == NULL) {
